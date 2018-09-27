c0e24cf802d51e3c3bda927ef34883397e7cdc7b
Allan <allan.hal@gmail.com>
2018-09-26 22:23:00 -0300

    Heroku

---
+++ app.js
@@ -0,0 +1,41 @@
+var compression = require('compression')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(compression())
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(express.static(__dirname + '/dist'))
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = err;
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
